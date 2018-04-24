351abfe7b7192b137b51936f73093610f34ae06d
Thomas BLEUSE <tbleuse@student.42.fr>
2018-04-24 17:08:25 +0200

    header changed

--- header/lem_in.h
+++ header/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: anaroste <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/24 15:10:33 by anaroste          #+#    #+#             */
-/*   Updated: 2018/04/24 15:28:28 by anaroste         ###   ########.fr       */
+/*   Updated: 2018/04/24 17:08:05 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-# include "./libft/header/libft.h"
+# include "../libft/header/libft.h"
 
 /*
 **			STRUCTURES
@@ -24,13 +24,27 @@
 typedef struct			s_txt
 {
 	char				*str;
-	struct stxt			*next;
+	struct s_txt		*next;
 }						t_txt;
+
+typedef struct			s_room
+{
+	char				*name;
+	int					id;
+	int					ant;
+	int					special;
+	int					on;
+	int					x;
+	int					y;
+	struct s_room		**liaison;
+	struct s_room		*next;
+}
 
 typedef struct			s_stock
 {
 	int					ant_nbr;
 	t_txt				*begin_txt;
+	t_room				*room;
 }						t_stock;
 
 /*
