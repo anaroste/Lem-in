8bde2dc48a4103d27103946ec33e1be170b9eeca
Thomas BLEUSE <tbleuse@student.42.fr>
2018-04-24 17:28:49 +0200

    main added

--- lemin_functions/main_lemin.c
+++ lemin_functions/main_lemin.c
@@ -6,12 +6,21 @@
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/24 15:25:10 by tbleuse           #+#    #+#             */
-/*   Updated: 2018/04/24 15:26:00 by tbleuse          ###   ########.fr       */
+/*   Updated: 2018/04/24 17:28:27 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../header/lem_in.h"
+
 int			main(int ac, char **av)
 {
-	ft_lemin(ac, av);
-	return (0);
+	t_stock		*s;
+
+	if (ac != 2)
+	{
+		ft_putstr("Wrong number of arguments\n");
+		return (0);
+	}
+	if (!(s = ft_lemin_read(av[1])))
+		return (0);
 }
