ee61098f7e6e2190b0fb407c3571b21a6c3ecb0a
Thomas BLEUSE <tbleuse@student.42.fr>
2018-04-25 17:22:34 +0200

    free_struct n'a pas du tout ete testee, mieux vaut ne pas l'utiliser pour l'instant

---
+++ lemin_functions/ft_free_struct.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_free_struct.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/25 17:06:47 by tbleuse           #+#    #+#             */
+/*   Updated: 2018/04/25 17:19:16 by tbleuse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/lem_in.h"
+
+int		ft_free_struct(t_stock *s)
+{
+	t_room *tmp;
+
+	tmp = NULL;
+	if (s->room)
+		tmp = s->room->next;
+	while (tmp)
+	{
+		free(s->room->name);
+		free(s->room->liaison);
+		free(s->room);
+		s->room = tmp;
+		tmp = tmp->next;
+	}
+	free(s->room->name);
+	free(s->room->liaison);
+	free(s->room);
+	free(s);
+	return (1);
+}
