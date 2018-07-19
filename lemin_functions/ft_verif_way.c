89262c7b05f960c84bf4af861d07023c552c7230
Thomas BLEUSE <tbleuse@e3r4p6.42.fr>
2018-07-19 16:19:21 +0200

    verification start--end

--- lemin_functions/ft_verif_way.c
+++ lemin_functions/ft_verif_way.c
@@ -6,7 +6,7 @@
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/24 15:25:10 by tbleuse           #+#    #+#             */
-/*   Updated: 2018/04/27 16:06:20 by tbleuse          ###   ########.fr       */
+/*   Updated: 2018/07/19 14:29:51 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_verif_way(t_stock *s)
 
 	i = -1;
 	while (s->start->liaison[++i])
-		if (s->start->liaison[i]->way)
+		if (s->start->liaison[i]->way || s->start->liaison[i]->special == 2)
 			return (1);
 	return (0);
 }
